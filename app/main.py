e68b802b1a9a9a2a0b1fb54a155d6c675b1351db
dylan <luodaojun1128@gmail.com>
2026-05-12 20:46:43 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def main():
         path_name = sha_hash[:2]
         file_name = sha_hash[2:]
 
-        with open(f"./.git/{path_name}/{file_name}", "rb") as file:
+        with open(f"./.git/objects/{path_name}/{file_name}", "rb") as file:
             raw = file.read()
         zlib.decompress(raw)
         print(raw)
